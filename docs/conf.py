b3d3151b42eb687d874f87e54885e23dbbd4f571
Ho Sung Lee <lee.ho.sung@gmail.com>
2018-10-01 13:36:54 +0900

    CHG: 공백 없애기

--- docs/conf.py
+++ docs/conf.py
@@ -134,6 +134,7 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
+    'extraclassoptions': 'openany,oneside'
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
